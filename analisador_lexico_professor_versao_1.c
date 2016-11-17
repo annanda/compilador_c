829ba6fe54d6985c0a9d88a0e3613ef0dc1fc899
Annanda Sousa <annanda.sousa@gmail.com>
2016-11-17 15:38:34 -0200

    arrumando identacao

--- analisador_lexico_professor_versao_1.c
+++ analisador_lexico_professor_versao_1.c
@@ -7,56 +7,56 @@
 char tokenVal[65000];
 
 int isDigit( int ch ) {
-  return ch >= '0' && ch <= '9';
+    return ch >= '0' && ch <= '9';
 }
 
 int isWS( int ch ) {
-  return ch == ' ' || ch == '\n'|| ch == '\t';
+    return ch == ' ' || ch == '\n'|| ch == '\t';
 }
 
 void erro( const char* msg ) {
-  printf( "%s\n", msg );
-  exit( 1 );
+    printf( "%s\n", msg );
+      exit( 1 );
 }
 
 int nextToken() {
-  int lookAhead = getchar();
-  int pos = 0;
+      int lookAhead = getchar();
+      int pos = 0;
 
-  while( isWS( lookAhead ) )
-    lookAhead = getchar();
-
-  if( isDigit( lookAhead ) ) {
-    while( isDigit( lookAhead ) ) {
-      tokenVal[pos++] = lookAhead;
-      lookAhead = getchar();
-    }
-
-    if( lookAhead == '.' ) {
-      tokenVal[pos++] = lookAhead;
+      while( isWS( lookAhead ) )
       lookAhead = getchar();
 
       if( isDigit( lookAhead ) ) {
-        while( isDigit( lookAhead ) ) {
+          while( isDigit( lookAhead ) ) {
           tokenVal[pos++] = lookAhead;
           lookAhead = getchar();
-	}
+      }
+
+      if( lookAhead == '.' ) {
+          tokenVal[pos++] = lookAhead;
+          lookAhead = getchar();
+
+          if( isDigit( lookAhead ) ) {
+            while( isDigit( lookAhead ) ) {
+              tokenVal[pos++] = lookAhead;
+              lookAhead = getchar();
+          }
       }
       else
-	erro( "Erro no numero: depois do ponto tem de "
-	      "ter pelo menos um digito" );
-    }
+        erro( "Erro no numero: depois do ponto tem de "
+              "ter pelo menos um digito" );
+        }
 
-    tokenVal[pos] = '\0';
-    return NUM;
-  }
+        tokenVal[pos] = '\0';
+        return NUM;
+    }
 }
 
 int main() {
-  int token = nextToken();
+    int token = nextToken();
 
-  printf( "%d\n", token );
-  printf( "%s\n", tokenVal );
+    printf( "%d\n", token );
+    printf( "%s\n", tokenVal );
 
-  return 0;
+    return 0;
 }
